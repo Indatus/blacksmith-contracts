da0eef324e95de291a271cb8e1331c5a347a8cb3
Michael Bunch <asher12000@gmail.com>
2014-06-04 09:49:21 -0400

    Fixed missing use statement calling RepositoryInterface. Reformatted for PSR-2.

--- hexagonal/app/lib/Repositories/DbRepository.php
+++ hexagonal/app/lib/Repositories/DbRepository.php
@@ -1,6 +1,7 @@
 <?php namespace Repositories;
 
 use Contracts\Instances\InstanceInterface;
+use Contracts\Repositories\RepositoryInterface;
 
 class DbRepository implements RepositoryInterface
 {
@@ -8,50 +9,56 @@ class DbRepository implements RepositoryInterface
     /**
      * @inheritDoc
      */
-    public function find($id) {
+    public function find($id)
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function findOrFail($id) {
+    public function findOrFail($id)
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function all() {
+    public function all()
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function update(InstanceInterface $model) {
+    public function update(InstanceInterface $model)
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function create(array $attributes) {
+    public function create(array $attributes)
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function save(InstanceInterface $model) {
+    public function save(InstanceInterface $model)
+    {
         //@todo modify with implementation
     }
 
     /**
      * @inheritDoc
      */
-    public function delete(InstanceInterface $model) {
+    public function delete(InstanceInterface $model)
+    {
         //@todo modify with implementation
     }
-
-} 
+}
